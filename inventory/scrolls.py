2ae5571b004f7e5fc7b653caee7dab61524e291f
Shades <Shades4355@gmail.com>
2020-11-27 15:33:47 -0500

    minor alterations for Scrolls

--- inventory/scrolls.py
+++ inventory/scrolls.py
@@ -1,9 +1,13 @@
-class Scroll(obj):
-    def __init__(self):
-        pass
+class Scroll(object):
+    def __init__(self, name):
+        self.name = name
 
 
 class Escape(Scroll):
+    """A scroll to temporarily escape combat"""
+    def __init__(self, name = "scroll: escape"):
+        self.name = name
+
     def escape(self):
         combat = False
         pass
